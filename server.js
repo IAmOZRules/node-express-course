9374e051a96fb05b19648ce72bbc5ca918025466
IAmOZRules <shreyaansnahata2001@gmail.com>
2021-08-27 12:04:54 +0530

    add first GET route

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const app = express();
 app.listen(8000, () => {
     console.log("Server is Running!");
 })
+
+const mockUserData = [
+    { name: "Mark" },
+    { name: "Jill" }
+]
+
+app.get("/users", (req, res) => {
+    res.json({
+        success: true,
+        message: 'Successfully got users. Nice!',
+        users = mockUserData,
+    })
+})
