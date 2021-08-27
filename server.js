0a6c2bde3584412fad8505c78d0f2351d401d70a
IAmOZRules <shreyaansnahata2001@gmail.com>
2021-08-27 12:06:56 +0530

    working first GET route

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000, () => {
-    console.log("Server is Running!");
-})
-
 const mockUserData = [
-    { name: "Mark" },
-    { name: "Jill" }
+    { name: 'Mark' },
+    { name: 'Jill' }
 ]
 
-app.get("/users", (req, res) => {
+app.get('/users', (req, res) => {
     res.json({
         success: true,
         message: 'Successfully got users. Nice!',
-        users = mockUserData,
+        users: mockUserData
     })
 })
+
+app.listen(8000, () => { console.log('Server is Listening') })
